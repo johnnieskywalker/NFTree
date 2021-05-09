ce9fe02afbe005ea67bda281aaf8336935c4e096
Lucas Johnnie <johnnie@pop-os.localdomain>
2021-05-09 17:27:55 +0200

    hard stop hackaton added printing hre config dont know how to use readArtifactSync in ts :/

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,6 +2,9 @@ import { ethers } from "hardhat";
 import { Root } from "../typechain/Root";
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { Contract } from "@ethersproject/contracts";
+// how to properly use hre want to use readArtifactSync in TS :/
+// import { HardhatRuntimeEnvironment } from "hardhat/types";
+const hre = require("hardhat");
 
 async function main() {
   const factory = await ethers.getContractFactory("Root");
@@ -12,10 +15,12 @@ async function main() {
   console.log("Deployment transaction hash: ", contract.deployTransaction.hash);
   // The contract is NOT deployed yet; we must wait until it is mined
   await contract.deployed();
+
+  generateFrontendFiles(contract);
 }
 
 function generateFrontendFiles(contract : Contract) {
-  const contractsDirectory = "/../frontend/src/contracts";
+  const contractsDirectory = __dirname+"/../frontend/src/contracts/";
 
   if (!existsSync(contractsDirectory)) {
     mkdirSync(contractsDirectory);
@@ -24,6 +29,11 @@ function generateFrontendFiles(contract : Contract) {
   writeFileSync(contractsDirectory + "contract-address.json", JSON.stringify({ "Root": contract.address }, undefined, 2));
 
   // TODO: JOH add making ABI for frontend like in https://github.com/nomiclabs/hardhat-hackathon-boilerplate/blob/master/scripts/deploy.js
+  // const RootArtifact = hre.readArtifactSync("Root");
+  console.log("Hardhat config");
+  console.log(hre.config);
+  
+
 }
 
 main()
